0f75ac99bd713b33721720b5ddaa21936005b997
SNEHARIITB <panabakasnehar@gmail.com>
2025-05-18 13:37:38 +0530

    sdg

--- Pagination/main.js
+++ Pagination/main.js
@@ -43,7 +43,7 @@ function renderTable(){
     });
     
     //const totalPages = Math.ceil(data.length / rowsPerPage);
-    pageinfo.textContent = `${currentPage}`;
+    pageinfo.textContent = `Page ${currentPage}`;
 
     //prevbtn.disabled = currentPage === 1;
     //nextbtn.disabled = currentPage === totalPages;
